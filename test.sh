e9a5c0bbec556701ad31b1f6754a4c475a1555ff
Superhac <superhac007@gmail.com>
2025-05-18 22:44:31 -0500

    move fss. batch#6

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Manually specify which directories to process
-roots=(2izbwpQR    7jN8q3noP6  BeZLx0uswb  EKW1u8qI    hgaAsEyx    kGBkVb-v    n5Cybri-    ptcOPvyW    s_qTTo6n    VrMnwhl6    YdN-rOSj \
-2mDm9-K67v  7M01q3g8uw  -bG4P570    eKx4rktS    _HhiKRagIK  kKUo_nB9K6  N7eK8Ph-    PUGkSJ80    SrA6feLw    VSodl7ur    YfwtEdJm \
-2OP1b0yoUV  7O0Ir6NU    bkbhaVNY    EM2muFci    -hk0ih1U    klXtdvkv    Na0QF2YG    Pwnbp9St    SrcagQN8    VSq_t7jP    Y-_IF4hA \
-2qcWVEi9    7RLpkFAp    Bl366m1T    EoB4gz82    hM7A-E0Z    KNUbC64Q    nGt78HB-    -PYamwZP    sRKscm_Phy  vT78ph4O    -YIQhPef \
-2rySSsJBBq  7yE7a51e    bMuvS7Qz    EoR-uZB74c  hMfM-Jpc    KPB534Xm0k  nJB_sFcW    pyhwSstJB6  _SuVoydb    VVMU7y7cmK  yKy2iXgI \
-2SoiVN-O    863y4Ku2HZ  Bn-4s9AS    EROqFKB-ih  hOQOmjB_    KreAUI99    nJdxFVxv    pYUzDR_B_L  s-uWDUDM    vyhtZif09Z  YPTXiOJJ \
+roots=(2v0eTaJ5    8aVmENVL    BQbsQc3p    Ewdd0S2y    hovL5QwV    KrfgLSR6    NjRUdQm9    -q0JBnQD    sv9glhK-    vyWVqHn5QF  y-qPSFTv9m \
+2vhzQ1t3    8c_hCqSaR2  bRgTaBG7    EWnMKr2a    HTQaWutn    KSCFPci7    nky-bYGY    q0RmE-GZ    SWT0OKYOwK  vZDUDUii    yXZFMWmA \
+2w21KV6ckm  8FRYLJJn    bSZ8yrs5    F1Skyh5h    hTVKuTdx    KulBN23mnk  nlhvszavbA  Q6mN43lD    swvDtOfV    VZTsPQCtxK  YYrA1tjB4i \
+2w2fS-yluY  8HG3mEVK    BunvWvh9    F4ma5afn    HUSCRwFAXS  kuQiQhLd    nOG72009    q8GzR2bQ    t2vMdwyv    w0F8K9Uc    YYWjYEbf \
+34pZqivK    8L4f2khJ    _bWc8eyo    F6QcJM6t_E  hWhboo3F    kv24rzg7    NOmu0MoJ    qaRU5of7    t3edtc1jQJ  w2p3HOPz    yZbH0Omg \
+37w__dnX    8La5MBsFtN  bY3FBoaUx3  F898-Sq-    Hx4NI4Lj    kvN44JGTQD  NqaGRv8k    qdNbj2SA    t4k9fe80kS  w33q-8UP    Z2B22VcP \
 )
 
 for dir in "${roots[@]}"; do
